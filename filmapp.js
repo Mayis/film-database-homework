bc2b2d0958f77b88424ee666c22865128c2c272e
Mayis <saroyan.mayis.ait@gmail.com>
2022-07-01 18:24:58 +0400

    new homework 7.1.2022

--- filmapp.js
+++ filmapp.js
@@ -7,7 +7,7 @@ const personalMovieDB = {
 	genres: [],
 	privat: false,
 	showMyDB() {
-		return personalMovieDB.privat ? personalMovieDB : `sorry its private`;
+		return personalMovieDB.privat ? `sorry its private` : personalMovieDB;
 	},
 	ratePersonByCount() {
 		if (personalMovieDB.count < 10) {
@@ -23,8 +23,9 @@ const personalMovieDB = {
 			let myGenres;
 			do {
 				myGenres = prompt(`${i + 1} : Ձեր նախընտրելի ժանրը`);
-			} while (!isNaN(myGenres));
-			personalMovieDB.genres.push(myGenres);
+				// additional way --!isNaN(myGenres) || myGenres.length > 20 || myGenres === null
+			} while (!/^\w\D{1,20}$/g.test(myGenres) || myGenres === null);
+			personalMovieDB.genres.push(myGenres.trim());
 		}
 	}
 };
@@ -46,11 +47,46 @@ function getFilmAndRate() {
 		} while (a === "" || a === null);
 		do {
 			b = +prompt(`${i + 1} : 1-10 ինչքա՞ն կգնահատեք այդ ֆիլմը`, "");
-		} while (b === 0 || isNaN(b));
+		} while (b <= 0 || isNaN(b) || b > 10);
 		a.trim();
 		personalMovieDB.movies[a] = b;
 	}
 }
 
-getFilmAndRate();
+personalMovieDB.yourFvoriteGenres();
 console.log(personalMovieDB);
+
+// some examples with destructuring
+
+let a, b;
+[a, b] = [1, 2];
+
+let c, d, all;
+[c, d, ...all] = [1, 2, 3, 4, 50];
+
+let arr = [1, [4, 5, 6], [7, 8, 9], 10];
+const [a1, [b1, b2, b3]] = arr;
+
+const obj = {
+	firstname: "Angel",
+	surname: "White",
+	age: 30,
+	maried: true,
+	children: {
+		boy: "alex",
+		girl: "Tamara",
+		isGoingSchool: true,
+		sports: {
+			boy: "boxing",
+			girl: "swiming"
+		}
+	}
+};
+const { sportBoy, SportGirl } = obj.children.sports;
+const { boy, girl } = obj.children;
+const { firstname, surname } = obj;
+
+function someArray(a, b, c) {
+	return [a, b, c];
+}
+const [first, second, third] = someArray(15, "Jack", false);
